69a647c7d18bd04b81c61a533bc2402d5ec92207
Dan Adams-Jacobson <dan@sliver.local>
2016-10-13 11:34:22 -0700

    Commit before we start doing the interesting stuff

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+/* Entry point, baby */
 import './polyfills.ts';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
-import { AppModule } from './app/';
+import { AppModule } from './app/'; /* implicitly loads from index.ts in this path */
 
 if (environment.production) {
   enableProdMode();
